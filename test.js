30f65e8674865fabf209affc2dcee9a235460478
Ignatius Thien <it@powerledger.io>
2020-09-16 23:08:23 +0800

    Added logic to loop through inputs and calculate bundles
    * Added logic to loop through each product containing code and size
    * Added logic to get matching product and sort bundles by size in descending order
    * Added logic to calculate bundle quantity, remaining size and total cost in reduce loop
    * Added logic to add all product summaries into a list

--- test.js
+++ test.js
@@ -14,6 +14,57 @@ const chunkArrayInTwos = arr => {
   return chunked;
 }
 
+const calcBundleOrder = inputs => {
+  const bundleResults = [];
+
+  inputs.forEach(val => {
+    const productItem = val.split(' ');
+
+    if (productItem.length === 2) {
+      const [productCode, size] = productItem;
+      const productIdx = store.findIndex(v => v.productCode === productCode);
+
+      if (productIdx !== -1) {
+        const { bundles } = store[productIdx];
+        const sortedBundles = bundles.sort((a, b) => b.size - a.size);
+        const parsedSize = parseFloat(size);
+        let tempSize = parsedSize;
+
+        const productResult = sortedBundles.reduce((acc, v) => {
+          const quantity = Math.floor(tempSize / v.size);
+          const remainder = tempSize % v.size;
+
+          if (quantity > 0) {
+            const remainingBundles = sortedBundles.filter(x => remainder % x.size === 0);
+
+            if (remainingBundles.length) {
+              tempSize = remainder;
+              const total = acc.total + (quantity * v.price);
+
+              return {
+                ...acc,
+                total: Math.round(total * 100) / 100,
+                bundles: [
+                  ...acc.bundles,
+                  { quantity, size: v.size, price: `$${v.price}` }
+                ]
+              };
+            }
+          }
+
+          return acc;
+        }, { size, productCode, total: 0, bundles: [] });
+
+        productResult.total = `$${productResult.total.toFixed(2)}`;
+
+        bundleResults.push(productResult);
+      }
+    }
+  });
+
+  return bundleResults;
+}
+
 if (args.length === 1 && args[0].split('.').pop() === 'txt') {
   fs.readFile(args[0], 'utf8', (err, data) => {
     if (err) {
@@ -22,7 +73,8 @@ if (args.length === 1 && args[0].split('.').pop() === 'txt') {
 
     if (data.length) {
       const items = data.split('\n');
-      console.log('item inputs from textfile', items);
+      const output = calcBundleOrder(items);
+      console.dir(output, { depth: null });
     }
   })
 } else {
