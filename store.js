be955b8f03a02420de1bc5bac092707d5b567124
Ignatius Thien <it@powerledger.io>
2020-09-16 22:36:50 +0800

    Defined mock store bundles and inputs
    * Added module to store mock bundle data
    * Added lines of sample inputs

---
+++ store.js
@@ -0,0 +1,36 @@
+const store = [{
+  productCode: 'HB24',
+  bundles: [{
+    size: 5,
+    price: 6.99
+  }, {
+    size: 10,
+    price: 12.99
+  }]
+}, {
+  productCode: 'PPM3',
+  bundles: [{
+    size: 3,
+    price: 9.95
+  }, {
+    size: 6,
+    price: 16.95
+  }, {
+    size: 9,
+    price: 24.95
+  }]
+}, {
+  productCode: 'BP19',
+  bundles: [{
+    size: 3,
+    price: 5.95
+  }, {
+    size: 5,
+    price: 9.75
+  }, {
+    size: 9,
+    price: 16.99
+  }]
+}];
+
+module.exports = { store };
